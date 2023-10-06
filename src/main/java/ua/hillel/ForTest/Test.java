c90f0ba46780cdcd913d1375063f8f83a6ff6c85
autodoc <alexeym@pkwteile.de>
2023-10-06 21:42:31 +0300

    lesson 33

--- src/main/java/ua/hillel/ForTest/Test.java
+++ src/main/java/ua/hillel/ForTest/Test.java
@@ -1,15 +1,10 @@
 package ua.hillel.ForTest;
 
+import java.util.stream.Collectors;
+
 public class Test {
     public static void main(String[] args) {
-        sdv();
-    }
 
-    public static void sdv() {
-        if (2 + 4 > 3) {
-            return;
-        }
-        System.out.println("qr");
     }
 }
 
--- src/test/java/lesson30/ashot/VisualRegressionTest.java
+++ src/test/java/lesson30/ashot/VisualRegressionTest.java
@@ -13,7 +13,6 @@ import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class VisualRegressionTest {
-
     @Test
     public void screenShot() throws IOException {
         open("https://www.google.com.ua/");
